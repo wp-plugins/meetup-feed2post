a25242c66d3f8d577aa81708073fe9b0bcd4273c
markparolisi <markparolisi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-09 16:55:04 +0000

    refactoring
    git-svn-id: https://plugins.svn.wordpress.org/meetup-feed2post/trunk@239067 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- meetup_feed2post.php
+++ meetup_feed2post.php
@@ -29,7 +29,6 @@ define('MEETUP_FEEDURL_OPT','meetup_feedurl');
 define('MEETUP_AUTHOR_OPT', 'meetup_author');
 
 
-//Setup Plugin...
 register_activation_hook(__FILE__, 'meetup_activation');
 add_action('meetup_schedule_event', 'meetup_insert_function');
 add_action('admin_menu', 'add_meetup_menu');
@@ -37,19 +36,19 @@ add_action('admin_notices', 'activation_notice');
 
 // Upon activation, schedule RSS fetching, create new category for posts, and add a DB field to store our url
 function meetup_activation() {
-    if(!has_action('meetup_schedule_event')):
+    if(!has_action('meetup_schedule_event')){
         wp_schedule_event(time(), 'daily', 'meetup_schedule_event');
-    endif;
-    wp_create_category("meetup"); 
+    }
+    wp_create_category("meetup");
     add_option(MEETUP_FEEDURL_OPT, 'meetup feed URL');
-   	add_option(MEETUP_AUTHOR_OPT, '1');
+    add_option(MEETUP_AUTHOR_OPT, '1');
 }
 
 //Alert Users to configure the plugin
 function activation_notice() {
-    if(get_option(MEETUP_FEEDURL_OPT) == 'meetup feed URL'):
+    if(get_option(MEETUP_FEEDURL_OPT) == 'meetup feed URL' || get_option(MEETUP_FEEDURL_OPT) == ''){
         echo '<div class="error fade"><p><strong>Please add your Meetup.com feed url to the admin page of this plugin located under the Settings category</strong></p></div>';
-    endif;
+    }
 }
 
 //Create admin panel and new category for our new posts
@@ -60,18 +59,18 @@ function add_meetup_menu() {
 //Admin Panel 
 function meetup_admin() {
     //Updates the FEED URL Option
-    if( isset($_POST["submit"]) ):
+    if( isset($_POST["update"]) ){
         remove_action('admin_notices', 'activation_notice');
         $feedurl = esc_url_raw($_POST["feedurl"]);
-	    $meetup_author = $_POST["user"];
+        $meetup_author = $_POST["user"];
         update_option(MEETUP_FEEDURL_OPT, $feedurl );
-   	  	update_option(MEETUP_AUTHOR_OPT, $meetup_author);
-        meetup_insert_function();
+        update_option(MEETUP_AUTHOR_OPT, $meetup_author);
+        meetup_insert_function(); //running our insert/update method on settings update
         echo "<p><div id='message' class='updated'>
     	<p><strong>Your settings have been updated</strong></p>
     	</div></p>";
-    endif;
-    $feedurl = esc_url(get_option(MEETUP_FEEDURL_OPT));
+    }
+    $current_feedurl = esc_url(get_option(MEETUP_FEEDURL_OPT));
     $current_author = get_option(MEETUP_AUTHOR_OPT);
     echo '
 		<div class="wrap">
@@ -79,11 +78,11 @@ function meetup_admin() {
 			<p><em>example: http://meetup.orlandophp.org/calendar/rss/The+Orlando+PHP+Meetup+Group</em></p>
 			<form method="POST" action="">
 			<label>RSS Feed URL</label>
-			<input type="text" name="feedurl" size="60" value="'.$feedurl,'"><br />
+			<input type="text" name="feedurl" size="60" value="'.$current_feedurl,'"><br />
 			<label>Assign to which author? </label>';
-			wp_dropdown_users('selected='.$current_author);
-	echo'	<br />
-			<input type="submit" name="submit" value="submit" />
+    wp_dropdown_users('selected='.$current_author);
+    echo'	<br />
+			<input type="submit" name="update" value="Update Options" class="button-primary" />
 			</form>	
 			</div>';
 
@@ -94,20 +93,20 @@ function meetup_insert_function() {
     global $wpdb;
     $feedurl = get_option(MEETUP_FEEDURL_OPT);
     $current_author = get_option(MEETUP_AUTHOR_OPT);
-   	$category_id = get_cat_id('meetup');
+    $category_id = get_cat_id('meetup');
     //loop through the current posts and store our custom meta GUID's into an array for dupe checking
     $guid_r = array();
     $existing_metas = $wpdb->get_results("SELECT * FROM $wpdb->postmeta WHERE meta_key = 'meetup_guid'");
-    foreach($existing_metas as $existing_meta):
+    foreach($existing_metas as $existing_meta){
         $guid = $existing_meta->meta_value;
         array_push($guid_r, $guid);
-    endforeach;
+    }
     //Create Object from Feed
     $meetup_rss = new SimpleXMLElement($feedurl, NULL, TRUE);
     //loop through each event from the feed
-    foreach($meetup_rss->xpath('channel/item') as $item):
+    foreach($meetup_rss->xpath('channel/item') as $item){
         //If we find a GUID match in our array, update the post
-        if(in_array($item->guid, $guid_r, false)):
+        if(in_array($item->guid, $guid_r, false)){
             $rss_guid = $item->guid;
             $rss_description = $item->description;
             $update_post_id = $wpdb->get_var($wpdb->prepare("SELECT post_id FROM $wpdb->postmeta WHERE meta_value='$rss_guid'"));
@@ -116,7 +115,7 @@ function meetup_insert_function() {
             $updated_info['post_author'] = $current_author;
             $updated_info['post_content'] = $rss_description;
             wp_update_post($updated_info);
-        else : //make the new post
+        } else{ //make the new post
             $post = array();
             $post['post_title'] = $item->title;
             $post['post_content'] = $item->description;
@@ -126,16 +125,15 @@ function meetup_insert_function() {
             $postID = wp_insert_post($post);
             //adding a unique custom meta field to check against duplicates
             add_post_meta($postID, 'meetup_guid', $item->guid);
-        endif;
-    endforeach;
+        }
+    }
 }
 
 //Stop scheduling events and remove Meetup Feed URL option on uninstall
 register_deactivation_hook(__FILE__, 'meetup_deactivation');
 function meetup_deactivation() {
-    if(has_action('meetup_schedule_event')):
+    if(has_action('meetup_schedule_event')){
         wp_clear_scheduled_hook('meetup_schedule_event');
-    endif;
+    }
     delete_option(MEETUP_FEEDURL_OPT);
 }
-?>
